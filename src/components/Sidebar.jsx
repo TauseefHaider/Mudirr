403b7626d486de5fc1a694257e0e17baafeaadc6
Tauseef Haider <haiderhad564@gmail.com>
2024-09-17 10:02:40 +0500

    Scroll

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import {
   Accordion,
@@ -25,10 +25,9 @@ import {
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 
-import { Divider } from '@chakra-ui/react'
-
+import { Divider } from "@chakra-ui/react";
 
 export default function Sidebar() {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -37,13 +36,12 @@ export default function Sidebar() {
     setSelectedIndex(index === selectedIndex ? 0 : index);
   };
 
-   const Ary = ["Dashboard", "Project Histoy", "Clint History", "Emails"]
+  const Ary = ["Dashboard", "Project Histoy", "Clint History", "Emails"];
 
   return (
     <Box w="304px" bg={"#F4F6FA"} py={"20px"} px={"16px"}>
-      
       <Image src="/logo.svg" width={116} height={39} alt="Logo" />
-      
+
       <Menu w={272}>
         <MenuButton
           as={Button}
@@ -162,111 +160,266 @@ export default function Sidebar() {
           </MenuItem>
         </MenuList>
       </Menu>
-      
+
       <Tabs variant="unstyled" mb="24px">
-        <TabList bg={"white"} padding={"4px"} borderRadius={"6px"} fontSize={"14px"} border={1} borderColor={"gray-600"} gap={"6px"} >
-          <Tab _selected={{ color: "#334876", bg: "#DBE2F0", fontWeight:"600"  }} w={"50%"} borderRadius={"6px"} color={"#707066"}>Personal</Tab>
-          <Tab  w={"50%"} _selected={{ color: "#334876", bg: " #DBE2F0", fontWeight:"600", }} borderRadius={"6px"} color={"#707066"}>Invited</Tab>
+        <TabList
+          bg={"white"}
+          padding={"4px"}
+          borderRadius={"6px"}
+          fontSize={"14px"}
+          border={1}
+          borderColor={"gray-600"}
+          gap={"6px"}
+        >
+          <Tab
+            _selected={{ color: "#334876", bg: "#DBE2F0", fontWeight: "600" }}
+            w={"50%"}
+            borderRadius={"6px"}
+            color={"#707066"}
+          >
+            Personal
+          </Tab>
+          <Tab
+            w={"50%"}
+            _selected={{ color: "#334876", bg: " #DBE2F0", fontWeight: "600" }}
+            borderRadius={"6px"}
+            color={"#707066"}
+          >
+            Invited
+          </Tab>
         </TabList>
-        <TabPanels>
-          
-        </TabPanels>
+        <TabPanels></TabPanels>
       </Tabs>
+      <Box height={"432px"} overflow={"scroll"} scrollBehavior={"smooth"}>
+        <Box color={"#707066"} bg={"#ebf0fa"} borderRadius={"10px"} p={4}>
+          <Accordion
+            defaultIndex={[0]}
+            mb={"10px"}
+            allowToggle
+            onChange={handleAccordionChange}
+          >
+            {Ary.map((item, index) => (
+              <AccordionItem key={item} border={"0px"}>
+                <h2>
+                  <AccordionButton
+                    _expanded={{
+                      bg: "white",
+                      color: "black",
+                      fontWeight: "600",
+                      borderRadius: "10px",
+                    }}
+                    onClick={() => handleAccordionChange(index)}
+                  >
+                    <Flex
+                      as="span"
+                      flex="1"
+                      alignItems={"center"}
+                      gap={"4px"}
+                      fontSize={"14px"}
+                    >
+                      <Image
+                        src={
+                          selectedIndex === index ? "iconfill.svg" : "icon.svg"
+                        }
+                        width={4}
+                        height={4}
+                      />
+                      {item}
+                    </Flex>
+                  </AccordionButton>
+                </h2>
+              </AccordionItem>
+            ))}
+          </Accordion>
 
-      <Box color={"#707066"} bg={"#ebf0fa"} borderRadius={"10px"} p={4}>
-      <Accordion defaultIndex={[0]} mb={"12px"} allowToggle onChange={handleAccordionChange}>
-      {Ary.map((item, index) => (
-        <AccordionItem key={item} border={"0px"}>
-          <h2>
-            <AccordionButton  _expanded={{ bg: 'white', color: 'black', fontWeight:"600", borderRadius:"10px"}} onClick={() => handleAccordionChange(index)}>
-              <Flex as='span' flex='1'  alignItems={"center"} gap={"4px"} fontSize={"14px"}>
-              <Image src={selectedIndex === index ? "iconfill.svg" : "icon.svg"} width={4} height={4}/>
-                {item}
+          <Divider />
+
+          <Accordion
+            defaultIndex={[0]}
+            allowMultiple
+            my={"2px"}
+            borderColor={"#ebf0fa"}
+          >
+            <AccordionItem>
+              <h2>
+                <AccordionButton gap={"4px"}>
+                  <AccordionIcon />
+                  <Flex
+                    as="span"
+                    justifyContent={"space-between"}
+                    flex="1"
+                    textAlign="left"
+                    fontSize={"14px"}
+                    fontWeight={600}
+                  >
+                    WORKSPACE
+                    <Text
+                      fontSize={"10px"}
+                      p={"2px"}
+                      px={"4px"}
+                      bg={"#dadad7"}
+                      fontWeight={600}
+                      borderRadius={"10px"}
+                      textAlign="right"
+                    >
+                      Coming Soon
+                    </Text>
+                  </Flex>
+                </AccordionButton>
+              </h2>
+              <AccordionPanel fontSize={"14px"} ml={"25px"} gap={"10px"}>
+                <Flex justify={"space-between"} p={"2px"}>
+                  Work Space Name 1{" "}
+                  <Image src={"chevron-down.svg"} width={4} height={4} />{" "}
+                </Flex>
+                <Flex justify={"space-between"} p={"2px"}>
+                  Work Space Name 2{" "}
+                  <Image src={"chevron-down.svg"} width={4} height={4} />{" "}
+                </Flex>
+                <Flex justify={"space-between"} p={"2px"}>
+                  Work Space Name 3{" "}
+                  <Image src={"chevron-down.svg"} width={4} height={4} />{" "}
+                </Flex>
+                <Text
+                  color={"blue"}
+                  ml={"30px"}
+                  mt={"8px"}
+                  alignSelf={"center"}
+                >
+                  See All
+                </Text>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+
+          <Divider />
+
+          <Accordion
+            defaultIndex={[0]}
+            allowMultiple
+            my={"2px"}
+            borderColor={"#ebf0fa"}
+          >
+            <AccordionItem>
+              <h2>
+                <AccordionButton gap={"4px"}>
+                  <AccordionIcon />
+                  <Flex
+                    as="span"
+                    justifyContent={"space-between"}
+                    flex="1"
+                    textAlign="left"
+                    fontSize={"14px"}
+                    fontWeight={600}
+                  >
+                    LAUNCHPAD
+                  </Flex>
+                </AccordionButton>
+              </h2>
+              <AccordionPanel fontSize={"14px"} ml={"15px"} gap={"10px"}>
+                <Flex justify={"space-between"} p={"2px"}>
+                  <Flex gap={"6px"} alignItems={"center"}>
+                    {" "}
+                    <Image src={"Logo 2.svg"} width={4} height={4} /> Business
+                    Name 1
+                  </Flex>{" "}
+                  <Image src={"chevron-down.svg"} width={4} height={4} />{" "}
+                </Flex>
+                <Flex justify={"space-between"} p={"2px"}>
+                  <Flex gap={"6px"} alignItems={"center"}>
+                    {" "}
+                    <Image src={"Logo 3.svg"} width={4} height={4} /> Business
+                    Name 2
+                  </Flex>{" "}
+                  <Image src={"chevron-down.svg"} width={4} height={4} />{" "}
+                </Flex>
+                <Flex justify={"space-between"} p={"2px"}>
+                  <Flex gap={"6px"} alignItems={"center"}>
+                    {" "}
+                    <Image src={"Logo 4.svg"} width={4} height={4} /> Business
+                    Name 3
+                  </Flex>{" "}
+                  <Image src={"chevron-down.svg"} width={4} height={4} />{" "}
+                </Flex>
+                <Text
+                  color={"blue"}
+                  ml={"30px"}
+                  mt={"8px"}
+                  alignSelf={"center"}
+                >
+                  See All
+                </Text>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        </Box>
+        <Accordion
+          defaultIndex={[0]}
+          allowMultiple
+          my={"2px"}
+          borderColor={"#ebf0fa"}
+        >
+          <AccordionItem>
+            <h2>
+              <AccordionButton gap={"4px"}>
+                <AccordionIcon />
+                <Flex
+                  as="span"
+                  justifyContent={"space-between"}
+                  flex="1"
+                  textAlign="left"
+                  fontSize={"14px"}
+                  fontWeight={600}
+                >
+                  PINNED PROJECT (3/3)
+                </Flex>
+              </AccordionButton>
+            </h2>
+            <AccordionPanel fontSize={"14px"} ml={"15px"} gap={"10px"}>
+              <Flex justify={"space-between"} p={"2px"}>
+                <Flex gap={"6px"} alignItems={"center"}>
+                  {" "}
+                  <Image src={"icon.svg"} width={4} height={4} /> Project Name 1
+                </Flex>{" "}
+                <Image src={"chevron-down.svg"} width={4} height={4} />{" "}
               </Flex>
-              
-              
-            </AccordionButton>
-          </h2>
-          
-        </AccordionItem>
-      ))}
-    </Accordion>
-
-    <Divider/>
-
-    <Accordion defaultIndex={[0]} allowMultiple my={"10px"} borderColor={"#ebf0fa"}>
-  <AccordionItem>
-    <h2>
-      <AccordionButton gap={"4px"}>
-      <AccordionIcon />
-        <Flex as='span' justifyContent={"space-between"} flex='1' textAlign='left' fontSize={"14px"} fontWeight={600}>
-          WORKSPACE
-          <Text fontSize={"10px"} p={"2px"} px={"4px"} bg={"#dadad7"} fontWeight={600} borderRadius={"10px"} textAlign="right">Coming Soon</Text>
-        </Flex>
-        
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4} fontSize={"14px"} ml={"25px"} gap={"10px"}>
-      <Flex  justify={"space-between"} p={"2px"}>Work Space Name 1 <Image src={"chevron-down.svg"} width={4} height={4}/> </Flex>
-      <Flex  justify={"space-between"} p={"2px"}>Work Space Name 2 <Image src={"chevron-down.svg"} width={4} height={4}/> </Flex>
-      <Flex  justify={"space-between"} p={"2px"}>Work Space Name 3 <Image src={"chevron-down.svg"} width={4} height={4}/> </Flex>
-      <Text color={"blue"} ml={"30px"} mt={"8px"} alignSelf={"center"}>See All</Text>
-    </AccordionPanel>
-  </AccordionItem>
-
-  
-</Accordion>
-
-<Divider/>
-
-<Accordion defaultIndex={[0]} allowMultiple my={"10px"} borderColor={"#ebf0fa"}>
-  <AccordionItem>
-    <h2>
-      <AccordionButton gap={"4px"}>
-      <AccordionIcon />
-        <Flex as='span' justifyContent={"space-between"} flex='1' textAlign='left' fontSize={"14px"} fontWeight={600}>
-          LAUNCHPAD
-          
-        </Flex>
-        
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4} fontSize={"14px"} ml={"15px"} gap={"10px"}>
-      <Flex  justify={"space-between"} p={"2px"}><Flex gap={"6px"} alignItems={"center"}> <Image src={"Logo 2.svg"} width={4} height={4} /> Business Name 1</Flex> <Image src={"chevron-down.svg"} width={4} height={4}/> </Flex>
-      <Flex  justify={"space-between"} p={"2px"}><Flex gap={"6px"} alignItems={"center"}> <Image src={"Logo 3.svg"} width={4} height={4} /> Business Name 2</Flex> <Image src={"chevron-down.svg"} width={4} height={4}/> </Flex>
-      <Flex  justify={"space-between"} p={"2px"}><Flex gap={"6px"} alignItems={"center"}> <Image src={"Logo 4.svg"} width={4} height={4} /> Business Name 3</Flex> <Image src={"chevron-down.svg"} width={4} height={4}/> </Flex>
-      <Text color={"blue"} ml={"30px"} mt={"8px"} alignSelf={"center"}>See All</Text>
-    </AccordionPanel>
-  </AccordionItem>
-
-  
-</Accordion>
-      
+              <Flex justify={"space-between"} p={"2px"}>
+                <Flex gap={"6px"} alignItems={"center"}>
+                  {" "}
+                  <Image src={"icon.svg"} width={4} height={4} /> Project Name 2
+                </Flex>{" "}
+                <Image src={"chevron-down.svg"} width={4} height={4} />{" "}
+              </Flex>
+              <Flex justify={"space-between"} p={"2px"}>
+                <Flex gap={"6px"} alignItems={"center"}>
+                  {" "}
+                  <Image src={"icon.svg"} width={4} height={4} /> Project Name 3
+                </Flex>{" "}
+                <Image src={"chevron-down.svg"} width={4} height={4} />{" "}
+              </Flex>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
       </Box>
 
-      <Accordion defaultIndex={[0]} allowMultiple my={"10px"} borderColor={"#ebf0fa"}>
-  <AccordionItem>
-    <h2>
-      <AccordionButton gap={"4px"}>
-      <AccordionIcon />
-        <Flex as='span' justifyContent={"space-between"} flex='1' textAlign='left' fontSize={"14px"} fontWeight={600}>
-          PINNED PROJECT (3/3)
-          
-        </Flex>
-        
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4} fontSize={"14px"} ml={"15px"} gap={"10px"}>
-      <Flex  justify={"space-between"} p={"2px"}><Flex gap={"6px"} alignItems={"center"}> <Image src={"icon.svg"} width={4} height={4} /> Project Name 1</Flex> <Image src={"chevron-down.svg"} width={4} height={4}/> </Flex>
-      <Flex  justify={"space-between"} p={"2px"}><Flex gap={"6px"} alignItems={"center"}> <Image src={"icon.svg"} width={4} height={4} /> Project Name 2</Flex> <Image src={"chevron-down.svg"} width={4} height={4}/> </Flex>
-      <Flex  justify={"space-between"} p={"2px"}><Flex gap={"6px"} alignItems={"center"}> <Image src={"icon.svg"} width={4} height={4} /> Project Name 3</Flex> <Image src={"chevron-down.svg"} width={4} height={4}/> </Flex>
-      
-    </AccordionPanel>
-  </AccordionItem>
-
-  
-</Accordion>
-
+      <Box>
+        <Box mt={2} mr={"15px"} fontSize={"14px"} ml={"30px"} gap={"10px"} bg={"#f4f6fa"}>
+          <Flex justify={"space-between"} p={"2px"}>
+            <Flex gap={"6px"} alignItems={"center"}>
+              {" "}
+              <Image src={"icon.svg"} width={4} height={4} /> Account Setting 
+            </Flex>{" "}
+            <Image src={"chevron-down.svg"} width={4} height={4} />{" "}
+          </Flex>
+          <Flex justify={"space-between"} p={"2px"}>
+            <Flex gap={"6px"} alignItems={"center"}>
+              {" "}
+              <Image src={"icon.svg"} width={4} height={4} /> Logout
+            </Flex>{" "}
+            <Image src={"chevron-down.svg"} width={4} height={4} />{" "}
+          </Flex>
+        </Box>
+      </Box>
     </Box>
   );
 }
